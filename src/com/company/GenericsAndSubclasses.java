0697be4f653c66452cd82e09b4b8e445eaac56e2
ds1619 <ds1619@ic.ac.uk>
2020-04-22 14:30:11 +0100

    GenericsAndSubclasses

---
+++ src/com/company/GenericsAndSubclasses.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class GenericsAndSubclasses {
+
+  public class A {
+
+  }
+
+  public class B extends A {
+
+  }
+
+
+  public static void main(String[] args) {
+
+    Set<B> setOfB = new HashSet<B>();
+    Set<? extends A> setOfA = setOfB;
+  }
+}
